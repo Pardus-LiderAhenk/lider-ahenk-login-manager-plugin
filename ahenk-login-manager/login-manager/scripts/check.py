603bca8c3a1275f850c3ae06dbaaf230f8aaae30
mine.dogan <mine.dogan@agem.com.tr>
2016-08-11 11:21:43 +0300

    added logs and log file for check script & removed root from user list if exist

--- ahenk-login-manager/login-manager/scripts/check.py
+++ ahenk-login-manager/login-manager/scripts/check.py
@@ -5,6 +5,8 @@
 import configparser
 import datetime
 import glob
+import logging
+import os
 import subprocess
 import sys
 
@@ -12,6 +14,12 @@ import sys
 class CheckTime():
     def __init__(self):
         super(self.__class__, self).__init__()
+
+        if not os.path.exists('{0}login-manager/logs'.format(sys.argv[1])):
+            os.makedirs('{0}login-manager/logs'.format(sys.argv[1]))
+
+        logging.basicConfig(filename='{0}login-manager/logs/check.log'.format(sys.argv[1]), filemode='w', level=logging.DEBUG)
+
         self.files = glob.glob('{0}login-manager/login_files/*.permissions'.format(sys.argv[1]))
 
         self.username = 'None'
@@ -35,6 +43,8 @@ class CheckTime():
         self.command_logout_user = 'pkill -u {0}'
         self.command_get_users_currently_login = "who | cut -d' ' -f1 | sort | uniq"
 
+        logging.debug('Parameters  were initialized.')
+
 
     def handle(self):
         try:
@@ -46,11 +56,13 @@ class CheckTime():
                 config_parser = configparser.ConfigParser()
                 config_parser.read(file)
 
+                logging.debug('Getting parameters from permission file for user \'{0}\''.format(self.username))
                 self.days = config_parser.get('PERMISSION', 'days')
                 self.start_time = config_parser.get('PERMISSION', 'start_time')
                 self.end_time = config_parser.get('PERMISSION', 'end_time')
                 self.last_date = datetime.datetime.strptime(str(config_parser.get('PERMISSION', 'last_date')),
                                                             "%Y-%m-%d").date()
+                logging.debug('Days: {0}, Start Time: {1}, End Time: {2}, Last Date: {3}'.format(self.days, self.start_time, self.end_time, self.last_date))
 
                 self.arr_start_time = str(self.start_time).split(':')
                 self.arr_end_time = str(self.end_time).split(':')
@@ -61,8 +73,10 @@ class CheckTime():
 
 
             if self.username != 'None':
+                logging.debug('Writing to user profile...')
                 self.write_to_user_profile()
             else:
+                logging.debug('Writing to global profile...')
                 self.write_to_global_profile()
 
         except Exception as e:
@@ -72,10 +86,12 @@ class CheckTime():
         if str(self.today) in self.days:
 
             if not (self.start_minute < self.current_minute < self.end_minute and self.current_date <= self.last_date):
+                logging.debug('User \'{0}\' will log out.'.format(self.username))
                 process = subprocess.Popen(self.command_logout_user.format(self.username), stdin=None, env=None, cwd=None, stderr=subprocess.PIPE,
                                            stdout=subprocess.PIPE, shell=True)
                 process.wait()
         else:
+            logging.debug('User \'{0}\' will log out.'.format(self.username))
             process = subprocess.Popen(self.command_logout_user.format(self.username), stdin=None, env=None, cwd=None, stderr=subprocess.PIPE,
                                        stdout=subprocess.PIPE, shell=True)
             process.wait()
@@ -92,15 +108,22 @@ class CheckTime():
             users = str(p_out).split('\n')
             users.pop()
 
+            if 'root' in users:
+                users.remove('root')
+
+        logging.debug('Logged-in users: {0}'.format(users))
+
         if str(self.today) in self.days:
 
             if not (self.start_minute < self.current_minute < self.end_minute and self.current_date <= self.last_date):
                 for user in users:
+                    logging.debug('User \'{0}\' will log out.'.format(user))
                     process = subprocess.Popen(self.command_logout_user.format(user), stdin=None, env=None, cwd=None, stderr=subprocess.PIPE,
                                                stdout=subprocess.PIPE, shell=True)
                     process.wait()
         else:
             for user in users:
+                logging.debug('User \'{0}\' will log out.'.format(user))
                 process = subprocess.Popen(self.command_logout_user.format(user), stdin=None, env=None, cwd=None, stderr=subprocess.PIPE,
                                            stdout=subprocess.PIPE, shell=True)
                 process.wait()
