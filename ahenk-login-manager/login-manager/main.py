02684f52e7807ca014cb37af8724221c2be965ae
Volkan Şahin <bm.volkansahin@gmail.com>
2016-10-06 17:08:38 +0300

    main module info added

--- ahenk-login-manager/login-manager/main.py
+++ ahenk-login-manager/login-manager/main.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
- 
+
 def info():
-    return None
+    inf = dict()
+    inf['name'] = 'login-manager'
+    inf['version'] = '1.0.0'
+    inf['support'] = 'debian'
+    inf['description'] = ''
+    inf['task'] = True
+    inf['user_oriented'] = True
+    inf['machine_oriented'] = True
+    inf['developer'] = ''
+
+    return inf
