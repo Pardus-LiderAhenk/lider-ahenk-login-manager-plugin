4d1be4eea5598e786d221e86ae7e7d23f2d5985d
Volkan Şahin <bm.volkansahin@gmail.com>
2016-10-11 16:52:59 +0300

    notifies were added

--- ahenk-login-manager/login-manager/scripts/check.py
+++ ahenk-login-manager/login-manager/scripts/check.py
@@ -10,15 +10,21 @@ import os
 import subprocess
 import sys
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
 
-class CheckTime():
+from base.util.util import Util
+from base.system.system import System
+
+
+class CheckTime:
     def __init__(self):
         super(self.__class__, self).__init__()
 
         if not os.path.exists('{0}login-manager/logs'.format(sys.argv[1])):
             os.makedirs('{0}login-manager/logs'.format(sys.argv[1]))
 
-        logging.basicConfig(filename='{0}login-manager/logs/check.log'.format(sys.argv[1]), filemode='w', level=logging.DEBUG)
+        logging.basicConfig(filename='{0}login-manager/logs/check.log'.format(sys.argv[1]), filemode='w',
+                            level=logging.DEBUG)
 
         self.files = glob.glob('{0}login-manager/login_files/*.permissions'.format(sys.argv[1]))
 
@@ -46,7 +52,6 @@ class CheckTime():
 
         logging.debug('Parameters  were initialized.')
 
-
     def handle(self):
         try:
 
@@ -66,7 +71,9 @@ class CheckTime():
                                                             "%Y-%m-%d").date()
                 self.duration = config_parser.get('PERMISSION', 'duration')
 
-                logging.debug('Days: {0}, Start Time: {1}, End Time: {2}, Last Date: {3}, Duration between notify and logout: {4}'.format(self.days, self.start_time, self.end_time, self.last_date, self.duration))
+                logging.debug(
+                    'Days: {0}, Start Time: {1}, End Time: {2}, Last Date: {3}, Duration between notify and logout: {4}'.format(
+                        self.days, self.start_time, self.end_time, self.last_date, self.duration))
 
                 self.arr_start_time = str(self.start_time).split(':')
                 self.arr_end_time = str(self.end_time).split(':')
@@ -74,7 +81,6 @@ class CheckTime():
                 self.start_minute = int(self.arr_start_time[0]) * 60 + int(self.arr_start_time[1])
                 self.end_minute = int(self.arr_end_time[0]) * 60 + int(self.arr_end_time[1])
                 self.current_minute = int(self.current_time.hour) * 60 + int(self.current_time.minute)
-
 
             if self.username != 'None':
                 logging.debug('Writing to user profile...')
@@ -91,23 +97,33 @@ class CheckTime():
 
             if not (self.start_minute < self.current_minute < self.end_minute and self.current_date <= self.last_date):
                 logging.debug('User \'{0}\' will log out.'.format(self.username))
-                process = subprocess.Popen(self.command_logout_user.format(self.username), stdin=None, env=None, cwd=None, stderr=subprocess.PIPE,
+                process = subprocess.Popen(self.command_logout_user.format(self.username), stdin=None, env=None,
+                                           cwd=None, stderr=subprocess.PIPE,
                                            stdout=subprocess.PIPE, shell=True)
                 process.wait()
 
-            elif (int(self.end_minute) - int(self.current_minute) == int(self.duration)):
+            elif int(self.end_minute) - int(self.current_minute) == int(self.duration):
+                Util.send_notify('Lider Ahenk',
+                                 'Oturum {0} dakika sonra kapatılacak!'.format(str(self.duration)),
+                                 System.Sessions.display(self.username),
+                                 self.username)
                 # notify ('Session will be terminated after "int(self.duration)" minute for user "self.username".')
-                pass
         else:
+            Util.send_notify('Lider Ahenk',
+                             'Oturum şimdi kapatılacak!',
+                             System.Sessions.display(self.username),
+                             self.username)
             # notify ('Session will be terminated now for user "self.username".')
             logging.debug('User \'{0}\' will log out.'.format(self.username))
-            process = subprocess.Popen(self.command_logout_user.format(self.username), stdin=None, env=None, cwd=None, stderr=subprocess.PIPE,
+            process = subprocess.Popen(self.command_logout_user.format(self.username), stdin=None, env=None, cwd=None,
+                                       stderr=subprocess.PIPE,
                                        stdout=subprocess.PIPE, shell=True)
             process.wait()
 
     def write_to_global_profile(self):
 
-        process = subprocess.Popen(self.command_get_users_currently_login, stdin=None, env=None, cwd=None, stderr=subprocess.PIPE,
+        process = subprocess.Popen(self.command_get_users_currently_login, stdin=None, env=None, cwd=None,
+                                   stderr=subprocess.PIPE,
                                    stdout=subprocess.PIPE, shell=True)
         process.wait()
         p_out = process.stdout.read().decode("unicode_escape")
@@ -127,18 +143,29 @@ class CheckTime():
             if not (self.start_minute < self.current_minute < self.end_minute and self.current_date <= self.last_date):
                 for user in users:
                     logging.debug('User \'{0}\' will log out.'.format(user))
-                    process = subprocess.Popen(self.command_logout_user.format(user), stdin=None, env=None, cwd=None, stderr=subprocess.PIPE,
+                    process = subprocess.Popen(self.command_logout_user.format(user), stdin=None, env=None, cwd=None,
+                                               stderr=subprocess.PIPE,
                                                stdout=subprocess.PIPE, shell=True)
                     process.wait()
 
-            elif (int(self.end_minute) - int(self.current_minute) == int(self.duration)):
+            elif int(self.end_minute) - int(self.current_minute) == int(self.duration):
+                for user in users:
+                    Util.send_notify('Lider Ahenk',
+                                     'Oturum {0} dakika sonra kapatılacak!'.format(str(self.duration)),
+                                     System.Sessions.display(user),
+                                     user)
                 # notify ('Session will be terminated after "int(self.duration)" minute for users: "users".')
                 pass
         else:
             for user in users:
+                Util.send_notify('Lider Ahenk',
+                                 'Oturum şimdi kapatılacak!',
+                                 System.Sessions.display(user),
+                                 user)
                 # notify ('Session will be terminated now for user "user".')
                 logging.debug('User \'{0}\' will log out.'.format(user))
-                process = subprocess.Popen(self.command_logout_user.format(user), stdin=None, env=None, cwd=None, stderr=subprocess.PIPE,
+                process = subprocess.Popen(self.command_logout_user.format(user), stdin=None, env=None, cwd=None,
+                                           stderr=subprocess.PIPE,
                                            stdout=subprocess.PIPE, shell=True)
                 process.wait()
 
